80079a36038ff53382306282f7be353697c3c28a
giuppep <35850759+giuppep@users.noreply.github.com>
2021-05-31 12:05:52 -0300

    Improved error handling (#13)
    * custom error types
    * move error to separate file

--- src/blob_store.rs
+++ src/blob_store.rs
@@ -1,23 +1,26 @@
 use crate::blob::BlobRef;
+use crate::error::{BlobError, Result};
 use ignore::{WalkBuilder, WalkState};
-use std::fs;
 use std::path::Path;
 use std::sync::mpsc;
+use std::{fs, io};
 
-pub fn add_file(path: &Path, verbose: bool) -> BlobRef {
-    assert!(path.is_file());
+pub fn add_file(path: &Path, verbose: bool) -> Result<BlobRef> {
+    if !path.is_file() {
+        return Err(BlobError::IO(io::Error::from(io::ErrorKind::InvalidInput)));
+    }
 
-    let blob_ref = BlobRef::from_path(path).unwrap();
+    let blob_ref = BlobRef::from_path(path)?;
     if !blob_ref.exists() {
         let save_path = &blob_ref.to_path();
-        fs::create_dir_all(save_path).expect("Could not create save directory");
+        fs::create_dir_all(save_path)?;
         let filename = path.file_name().unwrap();
-        fs::copy(path, save_path.join(&filename)).expect("Could not copy");
+        fs::copy(path, save_path.join(&filename))?;
     }
     if verbose {
         println!("{}\t{}", blob_ref.reference(), path.to_str().unwrap());
     }
-    blob_ref
+    Ok(blob_ref)
 }
 
 fn add_folder_multi_threaded(path: &Path, verbose: bool) -> Vec<BlobRef> {
@@ -29,8 +32,10 @@ fn add_folder_multi_threaded(path: &Path, verbose: bool) -> Vec<BlobRef> {
             Ok(entry) => {
                 let path = entry.path();
                 if path.is_file() {
-                    let blob_ref = add_file(path, verbose);
-                    tx.send(blob_ref).expect("Err");
+                    match add_file(path, verbose) {
+                        Ok(blob_ref) => tx.send(blob_ref).expect("Err"),
+                        Err(e) => eprintln!("{}", e),
+                    }
                 }
                 WalkState::Continue
             }
@@ -53,22 +58,27 @@ fn add_folder_single_threaded(path: &Path, verbose: bool) -> Vec<BlobRef> {
         if let Ok(entry) = entry {
             let path = entry.path();
             if path.is_file() {
-                let blob_ref = add_file(path, verbose);
-                blob_refs.push(blob_ref);
+                match add_file(path, verbose) {
+                    Ok(blob_ref) => blob_refs.push(blob_ref),
+                    Err(e) => eprintln!("{}", e),
+                }
             }
         }
     }
     blob_refs
 }
-pub fn add_folder(path: &Path, parallel: bool, verbose: bool) -> Vec<BlobRef> {
-    assert!(path.is_dir());
+pub fn add_folder(path: &Path, parallel: bool, verbose: bool) -> Result<Vec<BlobRef>> {
+    if !path.is_dir() {
+        return Err(BlobError::IO(io::Error::from(io::ErrorKind::InvalidInput)));
+    }
 
     let blob_refs = match parallel {
         true => add_folder_multi_threaded(path, verbose),
         false => add_folder_single_threaded(path, verbose),
     };
+
     if verbose {
         println!("Imported {} files", blob_refs.len());
     }
-    blob_refs
+    Ok(blob_refs)
 }
---
+++ src/error.rs
@@ -0,0 +1,40 @@
+use std::{fmt, io};
+
+#[derive(Debug)]
+pub enum BlobError {
+    IO(io::Error),
+    Blob(BlobErrorKind),
+}
+#[derive(Debug)]
+pub enum BlobErrorKind {
+    InvalidRefLength,
+    NotFound,
+}
+
+impl BlobErrorKind {
+    fn as_str(&self) -> &str {
+        match *self {
+            BlobErrorKind::NotFound => "File not found",
+            BlobErrorKind::InvalidRefLength => {
+                "Invalid refererence length. Reference must have 64 characters."
+            }
+        }
+    }
+}
+
+impl fmt::Display for BlobError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            BlobError::IO(ref err) => err.fmt(f),
+            BlobError::Blob(ref err) => write!(f, "Error: {}", err.as_str()),
+        }
+    }
+}
+
+impl From<io::Error> for BlobError {
+    fn from(err: io::Error) -> BlobError {
+        BlobError::IO(err)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, BlobError>;
