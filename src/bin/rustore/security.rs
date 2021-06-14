c340b35d6a1065bef082aab207bf7f0d4e7b5658
Giuseppe Papallo <giuseppe@papallo.it>
2021-06-14 16:38:58 -0300

    tidy up security module

--- src/bin/rustore/security.rs
+++ src/bin/rustore/security.rs
@@ -1,9 +1,15 @@
 use std::fs::{File, OpenOptions};
-use std::io::prelude::*;
-use std::path::Path;
+use std::io::{prelude::*, BufReader};
+use std::path::PathBuf;
 use uuid::Uuid;
 
-const TOKEN_STORE_PATH: &str = "/tmp/rustore/.tokens";
+const DEFAULT_TOKEN_STORE_PATH: &str = "/tmp/rustore/.tokens";
+
+fn token_store_path() -> PathBuf {
+    std::env::var("RUSTORE_TOKEN_STORE_PATH")
+        .unwrap_or_else(|_| DEFAULT_TOKEN_STORE_PATH.to_string())
+        .into()
+}
 
 /// Generates a new API token and appends it to the list of valid tokens.
 pub fn generate_token() -> String {
@@ -11,28 +17,28 @@ pub fn generate_token() -> String {
         .to_simple()
         .encode_upper(&mut Uuid::encode_buffer())
         .to_string();
-    save_token(&token, TOKEN_STORE_PATH);
+    save_token(&token);
     token
 }
 
 /// Appends a new token to the file containing the tokens.
-fn save_token<P: AsRef<Path>>(token: &str, token_store_path: P) {
+fn save_token(token: &str) {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(token_store_path)
+        .open(token_store_path())
         .expect("Can't open file.");
     writeln!(&mut file, "{}", token).unwrap();
 }
 
 /// Validates a token against a file containing a list of valid tokens.
 pub fn validate_token(token: &str) -> bool {
-    let file = match File::open(TOKEN_STORE_PATH) {
+    let file = match File::open(token_store_path()) {
         Ok(file) => file,
         Err(_) => return false,
     };
 
-    let reader = std::io::BufReader::new(file);
+    let reader = BufReader::new(file);
     for line in reader.lines() {
         if token == line.unwrap() {
             return true;
