2e885a3b7ba7159e41fdbe417025b198363c47fb
Giuseppe Papallo <giuseppe@papallo.it>
2021-06-19 13:43:55 -0300

    implement configuration via config file + default directories; closes #10 and #18

--- src/bin/rustore/settings.rs
+++ src/bin/rustore/settings.rs
@@ -1,29 +1,43 @@
-use directories::BaseDirs;
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
 use std::default::Default;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
-#[derive(Debug)]
+fn project_dirs() -> ProjectDirs {
+    ProjectDirs::from("", "", "rustore").unwrap()
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(default)]
 pub struct ServerSettings {
+    /// Port on which to run the rustore server
     pub port: u16,
+    /// Level of logging for rustore server
     pub log_level: log::Level,
+    /// Path to a directory where the server can store temporary files
     pub tmp_directory: PathBuf,
+    /// Path to a file containing a list of valid API tokens
     pub token_store_path: PathBuf,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(default)]
 pub struct Settings {
+    /// Path to the directory where all the blobs will be stored
     pub data_store_dir: PathBuf,
+    /// Server settings, see [`ServerSettings`]
     pub server: ServerSettings,
 }
 
 impl Default for ServerSettings {
     fn default() -> Self {
-        let dirs = BaseDirs::new().unwrap();
-        let token_store_path = dirs.config_dir().join("rustore").join(".tokens");
+        let token_store_path = project_dirs().config_dir().join(".tokens");
         ServerSettings {
             port: 3123,
             log_level: log::Level::Info,
-            tmp_directory: PathBuf::from("/tmp/rustore/"),
+            tmp_directory: std::env::temp_dir(),
             token_store_path,
         }
     }
@@ -31,8 +45,7 @@ impl Default for ServerSettings {
 
 impl Default for Settings {
     fn default() -> Self {
-        let dirs = BaseDirs::new().unwrap();
-        let data_store_dir = dirs.data_dir().join("rustore");
+        let data_store_dir = project_dirs().data_dir().into();
         Settings {
             data_store_dir,
             server: ServerSettings::default(),
@@ -40,12 +53,66 @@ impl Default for Settings {
     }
 }
 
+impl Settings {
+    /// Set the value of the environment variables based on the current configuration
+    pub fn set_env_vars(&self) {
+        std::env::set_var("RUSTORE_DATA_PATH", &self.data_store_dir);
+    }
+
+    /// Default path for the configuration file.
+    fn default_config_path() -> PathBuf {
+        project_dirs().config_dir().join("rustore.toml")
+    }
+
+    /// Load the rustore configuration from a `toml` file.
+    ///
+    /// # Examples
+    /// ```no_run
+    /// # use std::path::PathBuf;
+    /// let config_file = PathBuf::from("/home/giuppep/.config/rustore.toml")
+    /// let settings = Settings::from_file(config_file).unwrap()
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// The function will error if the file cannot be read or deserialized properly.
+    pub fn from_file(path: Option<PathBuf>) -> Result<Self, &'static str> {
+        // TODO: proper error handling
+        let path = path.unwrap_or(Settings::default_config_path());
+
+        let content = match std::fs::read_to_string(path) {
+            Ok(content) => content,
+            Err(_) => return Err("File cannot be read"),
+        };
+
+        match toml::from_str(&content) {
+            Ok(settings) => Ok(settings),
+            Err(_) => Err("cannot deserialize"),
+        }
+    }
+
+    /// Save the rustore configuration to a file. If the path is not specified, it
+    /// saves it to the default path.
+    pub fn to_file(&self, path: Option<PathBuf>) -> std::io::Result<()> {
+        let path = path.unwrap_or(Settings::default_config_path());
+
+        let toml_str = toml::to_string(&self).unwrap();
+        let mut file = File::create(&path)?;
+        file.write_all(&toml_str.as_bytes())?;
+        println!("Created config in {:?}", &path);
+
+        Ok(())
+    }
+}
+
 impl ServerSettings {
+    /// Set the value of the environment variables based on the current configuration
     pub fn set_env_vars(&self) {
         std::env::set_var("RUSTORE_TMP_FOLDER", &self.tmp_directory);
         std::env::set_var("RUSTORE_TOKEN_STORE_PATH", &self.token_store_path);
     }
 
+    /// Create all directories definied in the current configuration.
     pub fn create_dirs(&self) {
         std::fs::create_dir_all(&self.tmp_directory).unwrap();
         std::fs::create_dir_all(&self.token_store_path.parent().unwrap()).unwrap();
