f20998a9cc8dbee7ca9721802ad34b145ff430b7
Giuseppe Papallo <giuseppe@papallo.it>
2021-07-04 22:27:53 -0300

    error handling in token generation

--- rstr_server/src/security.rs
+++ rstr_server/src/security.rs
@@ -4,23 +4,23 @@ use std::path::Path;
 use uuid::Uuid;
 
 /// Generates a new API token and appends it to the list of valid tokens.
-pub fn generate_token(token_store_path: &Path) -> String {
+pub fn generate_token(token_store_path: &Path) -> std::io::Result<String> {
     let token = (*Uuid::new_v4()
         .to_simple()
         .encode_upper(&mut Uuid::encode_buffer()))
     .to_string();
-    save_token(&token, token_store_path);
-    token
+    save_token(&token, token_store_path)?;
+    Ok(token)
 }
 
 /// Appends a new token to the file containing the tokens.
-fn save_token(token: &str, token_store_path: &Path) {
+fn save_token(token: &str, token_store_path: &Path) -> std::io::Result<()> {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(token_store_path)
-        .expect("Can't open file.");
+        .open(token_store_path)?;
     writeln!(&mut file, "{}", token).unwrap();
+    Ok(())
 }
 
 /// Validates a token against a file containing a list of valid tokens.
